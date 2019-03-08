7e12e7be5af7e713605236baca0e2951ffcb69c5
Xavius123 <dantweigel@gmail.com>
2019-03-08 14:03:39 -0800

    test comm

---
+++ HomeRunBingo.Models/Domain/BingoCardDomain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeRunBingo.Models.Domain
+{
+    public class BingoCardDomain
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string card { get; set;}
+        public string square1 { get; set;}
+        public string square2 { get; set;}
+        public string square3 { get; set;}
+        public string square4 { get; set;}
+        public string square5 { get; set;}
+        public string square6 { get; set;}
+        public string square7 { get; set;}
+        public string square8 { get; set;}
+        public string square9 { get; set;}
+        public string square10 { get; set;}
+        public string square11 { get; set;}
+        public string square12 { get; set;}
+        public string square13 { get; set;}
+        public string square14 { get; set;}
+        public string square15 { get; set;}
+        public string square16 { get; set;}
+
+    }
+}
