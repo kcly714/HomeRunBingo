0212fa159a207243121e0e975a8220dae6ea9390
Xavius123 <dantweigel@gmail.com>
2019-03-07 23:13:11 -0800

    added login service

---
+++ HomeRunBingo.Models/Domain/ConfigDomain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeRunBingo.Models.Domain
+{
+    public class ConfigDomain
+    {
+        public int Id { get; set; }
+        public string ApiName { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
---
+++ HomeRunBingo.Models/Requests/RegisterAddRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeRunBingo.Models.Requests
+{
+    public class RegisterAddRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+        public string MiddleInitial { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Salt { get; set; }
+    }
+}
